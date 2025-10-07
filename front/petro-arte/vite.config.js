c5b9ac69b1c0d5a3ae0c9dc8934db0b1f6957cf7
Jona 6001 <jonathanrios601@gmail.com>
2025-10-07 17:34:21 -0600

    Actualización del build para GitHub Pages

--- front/petro-arte/vite.config.js
+++ front/petro-arte/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ESTADIAS1/',
+    build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 });
